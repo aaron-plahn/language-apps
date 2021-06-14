24f2f6c2cb7e1621b8fed04b114c65cc81f1b6f3
Aaron Plahn <aplahn85@gmail.com>
2021-06-13 22:03:42 -0700

    test(alphabet) fix a few failing tests

--- projects/alphabet/src/app/services/Alphabet/alphabet.service.spec.ts
+++ projects/alphabet/src/app/services/Alphabet/alphabet.service.spec.ts
@@ -1,12 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { AlphabetService } from './alphabet.service';
 
 describe('AlphabetService', () => {
   let service: AlphabetService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    // Inject the http service and test controller for each test
+    const httpClient = TestBed.get(HttpClient);
+    const httpTestingController = TestBed.get(HttpTestingController);
     service = TestBed.inject(AlphabetService);
   });
 
