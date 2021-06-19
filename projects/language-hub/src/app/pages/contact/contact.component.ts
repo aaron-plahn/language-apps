6360d8355409d63b9a096b15ccff7b29abb6f481
Aaron Plahn <aplahn85@gmail.com>
2021-06-19 12:23:10 -0700

    content(language-hub) apply minor updates to stylinng and content before publication

--- projects/language-hub/src/app/pages/contact/contact.component.ts
+++ projects/language-hub/src/app/pages/contact/contact.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../../types/Employee'; 
+import { Employee } from '../../types/Employee';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
   // TODO Pull this data from server
   employees: Employee[] = [
-    {'name': "Justin Bambrick", "title": "Front-End Developer"},
-    {'name': "Blake Sellars", "title": "Development Assistant"},
-    {'name':"Aaron Plahn", "title": "Technical Consultant"}
-  ]
+    { name: 'Justin Bambrick', title: 'Front-End Developer' },
+    { name: 'Riley Myers', title: 'IT Tech (dev ops)' },
+    { name: 'Blake Sellars', title: 'Development Assistant' },
+    { name: 'Aaron Plahn', title: 'Technical Consultant' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
