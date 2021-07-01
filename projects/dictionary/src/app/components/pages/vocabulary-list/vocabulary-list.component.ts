599f6d2e9319e85bdaa0e75a0049e7a847e9cf00
Aaron Plahn <aplahn85@gmail.com>
2021-07-01 15:43:05 -0700

    design(dictionary): create todo lists for front-end work

--- projects/dictionary/src/app/components/pages/vocabulary-list/vocabulary-list.component.ts
+++ projects/dictionary/src/app/components/pages/vocabulary-list/vocabulary-list.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AudioService } from 'audio';
 import { DictionaryDataService } from '../../../services/dictionary-data/dictionary-data.service';
-import { DictionarySearchService } from '../../../services/dictionary-search/dictionary-search.service';
-import { DropdownItem } from '../../widgets/dropdown/dropdown-item';
+import { TermWithValues } from '../../../services/dictionary-data/term-with-values';
 import { VocabularyList } from '../../../services/dictionary-data/vocabulary-list';
-import { ListVariable } from './list-variable';
+import { DictionarySearchService } from '../../../services/dictionary-search/dictionary-search.service';
 import { ListQuery } from '../../../services/dictionary-search/list-query';
 import { Parameter } from '../../../services/dictionary-search/parameter';
-import { TermWithValues } from '../../../services/dictionary-data/term-with-values';
-import { AudioService } from 'audio';
+import { DropdownItem } from '../../widgets/dropdown/dropdown-item';
+import { ListVariable } from './list-variable';
 
 @Component({
   selector: 'app-vocabulary-list',
   templateUrl: './vocabulary-list.component.html',
-  styleUrls: ['./vocabulary-list.component.css']
+  styleUrls: ['./vocabulary-list.component.css'],
 })
 export class VocabularyListComponent implements OnInit {
   selectedTerm: TermWithValues;
@@ -24,67 +24,77 @@ export class VocabularyListComponent implements OnInit {
   dropboxes: ListVariable<string>[] = [];
   checkboxes: ListVariable<boolean>[] = [];
 
-  constructor( private dictionaryData: DictionaryDataService, private dictionarySearch: DictionarySearchService, private route: ActivatedRoute, private audio: AudioService ) { }
+  constructor(
+    private dictionaryData: DictionaryDataService,
+    private dictionarySearch: DictionarySearchService,
+    private route: ActivatedRoute,
+    private audio: AudioService
+  ) {}
 
   ngOnInit(): void {
-  
-    this.dictionaryData.getTermsForListByListID("1").subscribe((terms: any)=>{
+    this.dictionaryData.getTermsForListByListID('1').subscribe((terms: any) => {
       this.terms = terms;
-      for(let term of terms){
+      for (let term of terms) {
         console.log(`id for this term is: ${term.id}`);
       }
     });
 
-    this.dictionaryData.getVocabularyListByID("1").subscribe((list: any)=>{
+    this.dictionaryData.getVocabularyListByID('1').subscribe((list: any) => {
       this.vocabularyList = list;
       console.log(`Here's the first dropbox:`);
       console.log(list.variables.dropboxes);
       this.setDropboxes(list.variables.dropboxes);
-    })
+    });
   }
 
-  handleNewSelection(data: DropdownItem<any>){
+  handleNewSelection(data: DropdownItem<any>) {
     console.log(`Variable options updated.`);
     console.log(`Data: ${data.value}`);
     console.log(`Display: ${data.display}`);
     this.search(this.createSearchQuery());
   }
 
-  private createSearchQuery(): ListQuery<any>{
+  private createSearchQuery(): ListQuery<any> {
     let q: ListQuery<any>;
     q = {
-      'parameters': []
-    }
-    if(!(this.dropboxes?.length || this.checkboxes?.length)) throw new Error(`Cannot create search query when both dropboxes and checkboxes are undefined.`);
-    if(this.dropboxes?.length){
-      for(let d of this.dropboxes){
+      parameters: [],
+    };
+    if (!(this.dropboxes?.length || this.checkboxes?.length))
+      throw new Error(
+        `Cannot create search query when both dropboxes and checkboxes are undefined.`
+      );
+    if (this.dropboxes?.length) {
+      for (let d of this.dropboxes) {
         let queryParameter: Parameter<string>;
         queryParameter = {
-          'name': d.name,
-          'value': d.currentValue.value
-        }
+          name: d.name,
+          value: d.currentValue.value,
+        };
         q.parameters.push(queryParameter);
       }
     }
-    if(this.checkboxes?.length){
-      for(let c of this.checkboxes){
+    if (this.checkboxes?.length) {
+      for (let c of this.checkboxes) {
         let queryParameter: Parameter<boolean>;
         queryParameter = {
-          'name': c.name,
-          'value': c.currentValue.value
-        }
+          name: c.name,
+          value: c.currentValue.value,
+        };
         q.parameters.push(queryParameter);
       }
     }
     return q;
   }
 
-  search(q: ListQuery<any>){
+  search(q: ListQuery<any>) {
     console.log(`Searching with query: ${q}`);
-    let result: TermWithValues = this.dictionarySearch.findOneUniqueTerm(q,this.terms);
-    if(result){
+    let result: TermWithValues = this.dictionarySearch.findOneUniqueTerm(
+      q,
+      this.terms
+    );
+    if (result) {
       console.log(`got a match: ${result}`);
-    } else{
+    } else {
       console.log(`No luck this time!`);
     }
     console.log(`selected result`);
@@ -92,20 +102,14 @@ export class VocabularyListComponent implements OnInit {
     this.selectedTerm = result;
   }
 
-  playAudio(){
-    let url: string = this.selectedTerm.term.audioURL;
-    this.audio.playAudioFromURL(this.selectedTerm.term.audioURL);
-  }
-
-  private setDropboxes(dropboxes){
-    if((typeof(dropboxes) === "undefined")) {
+  private setDropboxes(dropboxes) {
+    if (typeof dropboxes === 'undefined') {
       console.log(' NO DROPBOXES FOUND');
       return;
     }
 
-    for(let d of dropboxes){
-      this.dropboxes.push(new ListVariable(d,0,d.prompt,"dropbox"));
+    for (let d of dropboxes) {
+      this.dropboxes.push(new ListVariable(d, 0, d.prompt, 'dropbox'));
     }
   }
-
 }
--- projects/dictionary/src/app/components/widgets/term/term.component.ts
+++ projects/dictionary/src/app/components/widgets/term/term.component.ts
@@ -1,28 +1,36 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AudioService } from 'audio';
 import { DictionaryDataService } from '../../../services/dictionary-data/dictionary-data.service';
 import { Term } from '../../../services/dictionary-data/term';
 
 @Component({
   selector: 'app-term',
   templateUrl: './term.component.html',
-  styleUrls: ['./term.component.css']
+  styleUrls: ['./term.component.css'],
 })
 export class TermComponent implements OnInit {
-
   term: Term;
 
   _id: string;
-  @Input() public set termID(id:string){
-    if(!id && !(id === "0")) throw new Error(`termID is undefined`);
+  @Input() public set termID(id: string) {
+    if (!id && !(id === '0')) throw new Error(`termID is undefined`);
     this._id = id;
   }
-  constructor( private dictionaryDataService: DictionaryDataService ) { }
+  constructor(
+    private dictionaryDataService: DictionaryDataService,
+    private audio: AudioService
+  ) {}
 
   ngOnInit(): void {
-    this.dictionaryDataService.getTermByID(this._id).subscribe((term: Term)=>{
-      console.log(`Getting term with id:${this._id} for the term widget.`)
-      this.term =term;
-    })
+    this.dictionaryDataService.getTermByID(this._id).subscribe((term: Term) => {
+      console.log(`Getting term with id:${this._id} for the term widget.`);
+      this.term = term;
+    });
   }
 
+  playAudio() {
+    console.log('playing audio');
+    let url: string = this.term.audioURL;
+    this.audio.playAudioFromURL(this.term.audioURL);
+  }
 }
