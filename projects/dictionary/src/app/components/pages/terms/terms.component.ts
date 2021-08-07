490a3ec618bd904f3174eac4bb9f5a8e350ff7cb
Aaron Plahn <aplahn85@gmail.com>
2021-08-06 23:55:53 -0700

    refactor(dictionary): make table widget generic

--- projects/dictionary/src/app/components/pages/terms/terms.component.ts
+++ projects/dictionary/src/app/components/pages/terms/terms.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { DictionaryDataService } from '../../../services/dictionary-data/dictionary-data.service';
 import { Term } from '../../../services/dictionary-data/term';
-import { TermWithValues } from '../../../services/dictionary-data/term-with-values';
+import { TableData } from '../../widgets/table/table-data';
 
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.component.html',
-  styleUrls: ['./terms.component.css']
+  styleUrls: ['./terms.component.css'],
 })
 export class TermsComponent implements OnInit {
+  termsTable: TableData<Term>;
 
-  terms: Term[] = [];
-
-  constructor( private dictionaryDataService: DictionaryDataService ) { }
+  constructor(private dictionaryDataService: DictionaryDataService) {}
 
   ngOnInit(): void {
-    this.dictionaryDataService.getAllTerms().subscribe((data: Term[])=>{
-      this.terms = data;
-    })
+    this.dictionaryDataService.getAllTerms().subscribe((data: Term[]) => {
+      this.termsTable = this.getTermsTable(data);
+    });
   }
 
+  // TODO consider moving this higher and making it generic
+  getTermsTable(terms: Term[]): TableData<Term> {
+    return {
+      headings: ['term', 'termEnglish', 'contributor'],
+      rows: terms,
+    };
+  }
 }
--- projects/dictionary/src/app/components/widgets/table/table.component.ts
+++ projects/dictionary/src/app/components/widgets/table/table.component.ts
@@ -1,27 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Term } from '../../../services/dictionary-data/term';
+import { TableData } from './table-data';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
-export class TableComponent implements OnInit {
-  _terms: Term[];
+export class TableComponent<T extends Record<string, string>>
+  implements OnInit
+{
+  _data: TableData<T>;
 
-  @Input() public set terms(terms: Term[]) {
-    this._terms = terms;
+  @Input() public set data(data: TableData<T>) {
+    this._data = data;
   }
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  // myFunction2(data) {
-  //   console.log({ data });
-  // }
-
-  myFunction2(data) {
+  myFunction2(_data) {
     document.getElementById('demo').style.color = 'red';
   }
 }
